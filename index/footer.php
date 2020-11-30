b68bd88a72d8edef4e517af1988b25952959e7ad
NguyenThuHuyen173 <nguyenthuhuyenbts@gmail.com>
2020-11-30 13:36:37 +0700

    first commit

---
+++ index/footer.php
@@ -0,0 +1,15 @@
+<div class="footer">
+    <footer>
+        <table class="tb-footer">
+            <tr>
+                <td>Copyrights © 2020 Nguyễn Thu Huyền</td>
+            </tr>
+            <tr>
+                <td>Số điện thoại liên hệ: 0328875173</td>
+            </tr>
+            <tr>
+                <td>GMAIL: nguyenthuhuyenbts@gmail.com</td>
+            </tr>
+        </table>
+    </footer>
+</div>
